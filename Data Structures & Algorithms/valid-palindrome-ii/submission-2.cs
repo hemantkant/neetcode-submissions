e45f15ada00463fe224dbc73af123b00706c1747
hemantkant <54177498+hemantkant@users.noreply.github.com>
2026-04-26 23:31:07 +0530

    Add: valid-palindrome-ii - submission-2

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-2.cs
@@ -0,0 +1,30 @@
+// Optimal Approach (Same as Better)
+// Time & Space
+// TC: O(n)
+// SC: O(1)
+
+public class Solution {
+    public bool ValidPalindrome(string s) {
+    int l = 0, r = s.Length - 1;
+
+    while (l < r) {
+        if (s[l] == s[r]) {
+            l++;
+            r--;
+        } else {
+            return IsPalindrome(s, l + 1, r) || IsPalindrome(s, l, r - 1);
+        }
+    }
+
+    return true;
+}
+
+private bool IsPalindrome(string s, int l, int r) {
+    while (l < r) {
+        if (s[l] != s[r]) return false;
+        l++;
+        r--;
+    }
+    return true;
+}
+}
