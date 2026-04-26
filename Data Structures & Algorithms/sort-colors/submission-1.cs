b6cd027aad0635f365144ee3a53b6a301e263a87
hemantkant <54177498+hemantkant@users.noreply.github.com>
2026-04-26 23:54:09 +0530

    Add: sort-colors - submission-1

---
+++ Data Structures & Algorithms/sort-colors/submission-1.cs
@@ -0,0 +1,36 @@
+// Approach 3 — Optimal (Dutch National Flag Algorithm)
+// ⏱️ TC: O(n)
+// 🧠 SC: O(1)
+
+public class Solution {
+    public void SortColors(int[] nums)
+{
+    int low = 0, mid = 0, high = nums.Length - 1;
+
+    while (mid <= high)
+    {
+        if (nums[mid] == 0)
+        {
+            Swap(nums, low, mid);
+            low++;
+            mid++;
+        }
+        else if (nums[mid] == 1)
+        {
+            mid++;
+        }
+        else
+        {
+            Swap(nums, mid, high);
+            high--;
+        }
+    }
+}
+
+private void Swap(int[] nums, int i, int j)
+{
+    int temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+}
+}
