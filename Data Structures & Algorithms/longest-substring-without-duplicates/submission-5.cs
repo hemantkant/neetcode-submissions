2e270328a4f6ab57e74a7de36307154b53870aa7
hemantkant <54177498+hemantkant@users.noreply.github.com>
2026-05-01 23:55:41 +0530

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.cs
@@ -0,0 +1,29 @@
+// EVEN BETTER (Using Dictionary 🔥)
+// ⏱ TC: O(n)
+// 🧠 SC: O(n)
+
+// 👉 Instead of moving left one by one, jump directly
+
+// 💡 Idea:
+
+// Store last seen index of character
+
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+    var map = new Dictionary<char, int>();
+
+    int left = 0, maxLen = 0;
+
+    for (int right = 0; right < s.Length; right++) {
+
+        if (map.ContainsKey(s[right])) {
+            left = Math.Max(left, map[s[right]] + 1);
+        }
+
+        map[s[right]] = right;
+        maxLen = Math.Max(maxLen, right - left + 1);
+    }
+
+    return maxLen;
+    }
+}
